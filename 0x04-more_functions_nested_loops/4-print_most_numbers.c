7a28ad5944b5bc949fe32d2764d4796afec2862a
Moscowlee <hansonsamuel13@gmail.com>
2022-09-16 19:06:53 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,11 +10,12 @@
 void print_most_numbers(void)
 {
 int n;
-for(n = 0; n < 9; n++)
+
+for(n = 0; n <= 9; n++)
 {
 if ((n == 2) || (n == 4))
 continue;
 _putchar(n + 48);
 }
 _putchar('\n');
-}
+} 
