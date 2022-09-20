3538be3f4a49a1c04f150b482efa5eb429b48bdc
Moscowlee <hansonsamuel13@gmail.com>
2022-09-19 23:47:53 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,9 +2,7 @@
 
 /**
  * rev_string - this reverses a string
- *
  * @s: string parameter input
- *
  * Return: Nothing
  */
 
@@ -20,8 +18,7 @@ for (l = 0; s[l] != '\0'; ++l)
 /*swap the string by looping to half the string*/
 for (i = 0; i < l / 2; ++i)
 {
-	ch = s[i];									s[i] = s[l - 1 - i]; /*-1 because the array starts from 0*/
-	s[l - 1 - i] = ch;
-										}
-
+	ch = s[i];	
+	s[i] = s[l - 1 - i]; 
+	s[l - 1 - i] = ch;							}
 }
