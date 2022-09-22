8a45f0e52aa3962e13b0be2f72f1baba35d7404c
Moscowlee <hansonsamuel13@gmail.com>
2022-09-22 12:24:59 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,17 +10,13 @@
 
 char *_strcat(char *dest, char *src)
 {
-	int c, c2;
+	int index = 0, dest_len = 0;
 
-	c = 0;
-	/*find the size of dest array*/
-	while (dest[c])
-		c++
+	while (dest[index++])
+	dest_len++;
 
-	/* iterate through each src array value without the null byte*/
-	for (c2 = 0; src[c2] ; c2++)
-	/*append src[c2] to dest[c] while overwritting the null byte in dest*/
-	dest[c++] = src[c2];
+	for (index = 0; src[index]; index++)
+	dest[dest_len++] = src[index];
 
 	return (dest);
 }
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,10 +3,10 @@
 #include <stdio.h>
 
 /**
- *  *void prototypes()
+ * void prototypes()
  * int prototypes()
  * char prototypes()
- */
+*/
 void reverse_array(int *a, int n);
 void print_number(int n);
 void print_buffer(char *b, int size);
