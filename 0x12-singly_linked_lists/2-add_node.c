78599663384780b29a7479e2646d7942d20e7706
Moscowlee <hansonsamuel13@gmail.com>
2022-10-22 13:53:55 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -8,13 +8,12 @@
 
 size_t list_len(const list_t *h)
 {
-	size_t count;
-
-	count = 0;
-	while (h != NULL)
-	{
-		h = h->next;
-		count++;
-	}
-	return (count);
+	const list_t *tmp;
+	unsigned int i;
+	
+	
+	tmp = h;
+	for (i = 0; tmp; i++)
+		tmp = tmp->next;
+	return (i);
 }
